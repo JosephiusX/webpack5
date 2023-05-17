5a28d046e50790ab1881e22fe4f134e53585e163
josephius x <josephiusx@gmail.com>
2023-05-17 02:43:21 -0700

    Cannot GET /

--- wp-intro/webpack.config.js
+++ wp-intro/webpack.config.js
@@ -1,27 +1,22 @@
 const path = require('path')
 
 module.exports = {
-  context:path.resolve(__dirname,'src'),
-  // mode:'production',
-  // devtool:'source-map',
-  mode: 'development', 
-  devtool: 'inline-source-map',
-  // entry:'./entry.js',
-  entry:{
-    main:'./entry.js',
-  },
-  output:{
-    // iife:false,
-    clean:true,
-    filename:'[name].js',
-    path:path.resolve(__dirname, 'dist')
-  },
-  watchOptions:{
-    // ignored:/node_modules/
-    // ignored:[path.resolve(__dirname,'node_modules')]
-    ignored:['**/node_modules'] // glob pattern excludes node modules like options above
-  },
-  devServer:{
-    port:8080,hot:true,
-  }
+    context:path.resolve(__dirname,'src'),
+    mode:'development',
+    devtool:'inline-source-map',
+    entry:{
+        main:'./entry.js'
+    },
+    output:{
+        iife:true,
+        clean:true,
+        filename:'[name].js',
+        path:path.resolve(__dirname,'dist')
+    },
+    watchOptions:{
+        ignored:['**/node_modules']
+    },
+    devServer:{
+      port:8080,hot:true,
+    }
 }
