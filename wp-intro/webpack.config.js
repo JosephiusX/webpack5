3bac01d07c61aeb1af735e98c6d1c02a98947772
josephius x <josephiusx@gmail.com>
2023-05-17 01:52:45 -0700

    working but with typo I cant find the source of

--- wp-intro/webpack.config.js
+++ wp-intro/webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path')
 
 module.exports = {
   context:path.resolve(__dirname,'src'),
-  mode:'production',
-  devtool:'source-map',
-  // mode: 'development', 
-  // devtool: 'inline-source-map',
+  // mode:'production',
+  // devtool:'source-map',
+  mode: 'development', 
+  devtool: 'inline-source-map',
   // entry:'./entry.js',
   entry:{
     main:'./entry.js',
@@ -16,10 +16,12 @@ module.exports = {
     filename:'[name].js',
     path:path.resolve(__dirname, 'dist')
   },
-  watch:true, //--watch
   watchOptions:{
     // ignored:/node_modules/
     // ignored:[path.resolve(__dirname,'node_modules')]
-    ignored:['**/node_modules','**/src/info.js'] // glob pattern excludes node modules like options above
+    ignored:['**/node_modules'] // glob pattern excludes node modules like options above
+  },
+  devServer:{
+    port:8080,hot:true,
   }
 }
